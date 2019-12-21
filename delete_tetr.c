44d2f9cd118a7a8f13a8a4043850a93e3aa06778
Libbie Tammie <ltammie@vo-g4.21-school.ru>
2019-12-21 20:18:54 +0300

    g

--- delete_tetr.c
+++ delete_tetr.c
@@ -6,22 +6,21 @@
 /*   By: ltammie <ltammie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/21 19:55:05 by ltammie           #+#    #+#             */
-/*   Updated: 2019/12/21 20:02:38 by ltammie          ###   ########.fr       */
+/*   Updated: 2019/12/21 20:18:42 by ltammie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-//static t_tetr	*pop_tetr(t_tetr **head)
-//{
-//
-//}
-
 void	delete_tetr(t_tetr **head)
 {
+	t_tetr *prev = NULL;
+
 	while ((*head)->next)
 	{
-		free((*head));
+		prev = *head;
 		(*head) = (*head)->next;
+		free(prev);
 	}
+	free(*head);
 }
